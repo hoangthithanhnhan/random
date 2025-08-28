539e0c7d4b7ff98076128d1af3b63fafa740d63d
hoangthithanhnhan <nhan95dd2017@gmail.com>
2025-08-28 17:27:18 +0700

    init

---
+++ src/app/random/spin/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import InputColumns from "@/components/ColumnInput";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen">
+      bavc 
+    </div>
+  );
+}
